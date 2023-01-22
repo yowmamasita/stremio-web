6bef8b1c771b0218fc5de84168b196bee8d4d421
Ben Sarmiento <ben.sarmiento@forto.com>
2023-01-22 14:07:10 +0000

    error catching

--- src/routes/MetaDetails/StreamsList/Stream/external.js
+++ src/routes/MetaDetails/StreamsList/Stream/external.js
@@ -1,11 +1,14 @@
-const playViaDeepLink = (player, href) => deepLinks.externalPlayer && typeof deepLinks.externalPlayer.href === 'string' ? player +
-    Buffer.from(
-        href.split("base64,")[1].split('"')[0],
-        "base64"
-    )
-    .toString()
-    .split("https")[1] : null;
+const playViaDeepLink = (player, deepLinks) => deepLinks &&
+	deepLinks.externalPlayer &&
+	deepLinks.externalPlayer.href &&
+	typeof deepLinks.externalPlayer.href === 'string' ?
+		(deepLinks.externalPlayer.href.startsWith('data:') ?
+			player + Buffer.from(deepLinks.externalPlayer.href.split("base64,")[1].split('"')[0],"base64").toString().split("https")[1]
+			:
+			deepLinks.externalPlayer.href)
+		:
+		null;
 
 module.exports = {
-    playViaDeepLink
+	playViaDeepLink
 };
