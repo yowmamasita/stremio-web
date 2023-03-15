10e557399059e5c215d5012950413ba46c4690ae
Ben Sarmiento <ben.sarmiento@forto.com>
2023-03-15 11:46:01 +0000

    start script update 2

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 4446:80 -v $(pwd)/build:/usr/share/nginx/html --name stremio-web sarmiento/appletv:latest
+docker run -d -p 4446:80 -v $(pwd)/build:/usr/share/nginx/html --name stremio-atv sarmiento/appletv:latest
